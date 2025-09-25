7aaf98d86ffbd953bc43045e3b2f6ad2aa5c2022
Project516 <138796702+Project516@users.noreply.github.com>
2025-09-25 11:57:38 +0000

    add prebuilt jars

--- build_all.bat
+++ build_all.bat
@@ -1,10 +1,11 @@
 @echo off
+call gradlew build
 call gradlew core:shadowJar backend-rpc-core:shadowJar rewind_v1_5:shadowJar eaglermotd:shadowJar eaglerweb:shadowJar plan:shadowJar supervisor-core:shadowJar
-copy "core\build\libs\EaglerXServer.jar" "EaglerXServer.jar"
-copy "backend-rpc-core\build\libs\EaglerXBackendRPC.jar" "EaglerXBackendRPC.jar"
-copy "rewind_v1_5\build\libs\EaglerXRewind.jar" "EaglerXRewind.jar"
-copy "eaglermotd\build\libs\EaglerMOTD.jar" "EaglerMOTD.jar"
-copy "eaglerweb\build\libs\EaglerWeb.jar" "EaglerWeb.jar"
-copy "plan\build\libs\EaglerXPlan.jar" "EaglerXPlan.jar"
-copy "supervisor-core\build\libs\EaglerXSupervisor.jar" "EaglerXSupervisor.jar"
+copy "core\build\libs\EaglerXServer.jar" "jars/EaglerXServer.jar"
+copy "backend-rpc-core\build\libs\EaglerXBackendRPC.jar" "jars/EaglerXBackendRPC.jar"
+copy "rewind_v1_5\build\libs\EaglerXRewind.jar" "jars/EaglerXRewind.jar"
+copy "eaglermotd\build\libs\EaglerMOTD.jar" "jars/EaglerMOTD.jar"
+copy "eaglerweb\build\libs\EaglerWeb.jar" "jars/EaglerWeb.jar"
+copy "plan\build\libs\EaglerXPlan.jar" "jars/EaglerXPlan.jar"
+copy "supervisor-core\build\libs\EaglerXSupervisor.jar" "jars/EaglerXSupervisor.jar"
 pause
--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+./gradlew build
 ./gradlew core:shadowJar backend-rpc-core:shadowJar rewind_v1_5:shadowJar eaglermotd:shadowJar eaglerweb:shadowJar plan:shadowJar supervisor-core:shadowJar
-cp "core/build/libs/EaglerXServer.jar" "EaglerXServer.jar"
-cp "backend-rpc-core/build/libs/EaglerXBackendRPC.jar" "EaglerXBackendRPC.jar"
-cp "rewind_v1_5/build/libs/EaglerXRewind.jar" "EaglerXRewind.jar"
-cp "eaglermotd/build/libs/EaglerMOTD.jar" "EaglerMOTD.jar"
-cp "eaglerweb/build/libs/EaglerWeb.jar" "EaglerWeb.jar"
-cp "plan/build/libs/EaglerXPlan.jar" "EaglerXPlan.jar"
-cp "supervisor-core/build/libs/EaglerXSupervisor.jar" "EaglerXSupervisor.jar"
+cp "core/build/libs/EaglerXServer.jar" "jars/EaglerXServer.jar"
+cp "backend-rpc-core/build/libs/EaglerXBackendRPC.jar" "jars/EaglerXBackendRPC.jar"
+cp "rewind_v1_5/build/libs/EaglerXRewind.jar" "jars/EaglerXRewind.jar"
+cp "eaglermotd/build/libs/EaglerMOTD.jar" "jars/EaglerMOTD.jar"
+cp "eaglerweb/build/libs/EaglerWeb.jar" "jars/EaglerWeb.jar"
+cp "plan/build/libs/EaglerXPlan.jar" "jars/EaglerXPlan.jar"
+cp "supervisor-core/build/libs/EaglerXSupervisor.jar" "jars/EaglerXSupervisor.jar"
